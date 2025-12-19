25eb3b62c585092a03c4e5553784975211f8a346
mrcoder36 <mr.coder36@gmail.com>
2025-12-19 20:22:05 +0530

    Add whileTask class for user input and counting display

---
+++ src/tasks/whileTask.java
@@ -0,0 +1,19 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class whileTask {
+    public static void main(String args[]) {
+		Scanner data = new Scanner(System.in);
+		System.out.print("Enter your name: ");		
+		String name = data.next();
+		System.out.print("Enter the count: ");
+		int numb = data.nextInt();
+        int count = 0;
+		
+		while(count <= numb) {
+			System.out.println(count + "." + name);
+			count++;
+		}
+	}
+}
