c2e008730a1aaf94182f109b429b62defca991aa
mrcoder36 <mr.coder36@gmail.com>
2025-12-20 21:43:30 +0530

    Add timer class to print "hello World.." 101 times

---
+++ src/tasks/timer.java
@@ -0,0 +1,11 @@
+package tasks;
+
+public class timer {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num <= 100) {
+            System.out.println("hello World..");
+            num++;
+        }
+    }
+}
