e73ca13b0bd2d760d5d690ea09fc8d9989e22f91
mrcoder36 <mr.coder36@gmail.com>
2025-12-22 10:36:40 +0530

    Fix spacing in pattern class output

--- src/tasks/pattern.java
+++ src/tasks/pattern.java
@@ -4,7 +4,7 @@ public class pattern {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.print("* ");
+                System.out.print(" * ");
             }
             System.out.println();
         }
