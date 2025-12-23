a05635b3c70c3e8900503d8fc4083c3818b6362f
mrcoder36 <mr.coder36@gmail.com>
2025-12-23 19:20:10 +0530

    Add stringIspresent and stringLength classes for string manipulation tasks

---
+++ src/tasks/stringIspresent.java
@@ -0,0 +1,22 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class stringIspresent {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String name = data.next();
+
+        System.out.print("Enter the Character: ");
+        String letter = data.next();
+
+        boolean ispresent = name.contains(letter);
+
+        if (ispresent == true) {
+            System.out.println("Character is present...");
+        } else {
+            System.out.println("Character not present...");
+        }
+    }
+}
---
+++ src/tasks/stringLength.java
@@ -0,0 +1,18 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class stringLength {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = data.next();
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+        }
+        System.out.println("The count is " + count);
+    }
+}
