7496285a502c65165cb1de191fc94bdd5cc71872
mrcoder36 <mr.coder36@gmail.com>
2025-12-24 22:11:15 +0530

    Add stringWoCharAt class for character retrieval by index in a string

---
+++ src/tasks/stringWoCharAt.java
@@ -0,0 +1,19 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class stringWoCharAt {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = data.next();
+
+        System.out.print("Enter the index: ");
+        short index = data.nextShort();
+
+        char charArr[] = str.toCharArray();
+        System.out.println("The character at index " + index + " is " + charArr[index]);
+
+
+    }
+}
