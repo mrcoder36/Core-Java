18ef4b979c2e1628ac2bcbd1cddf891b0e4f8264
mrcoder36 <mr.coder36@gmail.com>
2025-12-24 22:11:45 +0530

    Add stringWoContains class to check character presence in a string

---
+++ src/tasks/stringWoContains.java
@@ -0,0 +1,31 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class stringWoContains {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        
+        System.out.print("Enter the string: ");
+        String str = data.next();
+
+        System.out.print("Enter the character: ");
+        char ch = data.next().charAt(0);
+
+        char charArr[] = str.toCharArray();
+        boolean isPresent = false;
+
+        for (int i = 0; i < charArr.length; i++) {
+            if (charArr[i] == ch) {
+                isPresent = true;
+            }
+        }
+
+        if (isPresent == true) {
+            System.out.println("The character is present...");
+        } else {
+            System.out.println("The character is not present..");
+        }
+    }
+
+}
