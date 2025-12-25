d06bdd23823f2c0e07fc29605f0a81f178406a85
mrcoder36 <mr.coder36@gmail.com>
2025-12-25 22:55:50 +0530

    Add stringBuilder class to demonstrate StringBuilder usage

---
+++ src/tasks/stringBuilder.java
@@ -0,0 +1,10 @@
+package tasks;
+
+public class stringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb);
+
+    }
+}
