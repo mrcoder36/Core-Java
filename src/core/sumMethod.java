9c9e6c9c777a29585ba6d61928a78435e0a27d0d
mrcoder36 <mr.coder36@gmail.com>
2025-12-28 23:21:14 +0530

    Add sumMethod class to perform basic arithmetic operations

---
+++ src/core/sumMethod.java
@@ -0,0 +1,50 @@
+package core;
+
+import java.util.Scanner;
+
+public class sumMethod {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter the 1st number: ");
+        int n = data.nextInt();
+        System.out.print("Enter the 2nd number: ");
+        int m = data.nextInt();
+
+        System.out.println("choose: + , - , * , /");
+        String choice = data.next();
+        data.close();
+        switch (choice) {
+            case "+":
+                System.out.println("The addition is " + add(n, m));
+                break;
+            case "-":
+                System.out.println("The subtraction is " + sub(n, m));
+                break;
+            case "*":
+                System.out.println("The multiplication is " + multi(n, m));
+                break;
+            case "/":
+                System.out.println("The division is " + div(n, m));
+                break;
+            default:
+                System.out.println("Invalid Input..");
+                break;
+        }
+        
+    }
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int sub(int a, int b) {
+        return a - b;
+    }
+
+    static int multi(int a, int b) {
+        return a * b;
+    }
+
+    static int div(int a, int b) {
+        return a / b;
+    }
+}
