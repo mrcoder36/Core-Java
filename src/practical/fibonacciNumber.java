a85761258b38918bd7fcec87d2afaa20ea7e6246
mrcoder36 <mr.coder36@gmail.com>
2026-01-02 16:01:32 +0530

    Add fibonacciNumber class to demonstrate Fibonacci sequence generation

---
+++ src/practical/fibonacciNumber.java
@@ -0,0 +1,22 @@
+package practical;
+
+public class fibonacciNumber {
+    public static void main(String[] args) {
+        fibonacciNumber obj = new fibonacciNumber();
+        obj.fib();
+    }
+
+    void fib() {
+        int num1 = 0;
+        int num2 = 1;
+        int num3;
+        for (int i = 1; i < 15; i++) {
+            num3 = num1 + num2;
+            System.out.println(num3);
+            
+            num1 = num2;
+            num2 = num3;
+        }
+    }
+
+}
