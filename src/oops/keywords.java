677b75dc3efcf1593245100bca450b515561770c
mrcoder36 <mr.coder36@gmail.com>
2026-01-02 15:08:10 +0530

    Add keywords class to demonstrate the use of final variables and class inheritance in Java

---
+++ src/oops/keywords.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class keywords {
+
+    class first {
+        // final void details() {
+
+        // }
+    }
+    
+    class second extends first{
+        void details() {
+
+        }
+    }
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+        // a = 30;
+        System.out.println(a);
+    }
+}
